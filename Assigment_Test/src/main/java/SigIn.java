4e2f6e3a0701e5a57105be7a530706ae87249bf4
yenhtt <thanhyen107@gmail.com>
2021-04-11 17:07:05 +0700

    update + reformat code

--- Assigment_Test/src/main/java/SigIn.java
+++ Assigment_Test/src/main/java/SigIn.java
@@ -1,9 +1,9 @@
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 
@@ -18,21 +18,17 @@ class SignIn {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-
         WebElement registerBtn = driver.findElement(By.xpath("//span[contains(text(),'Register/Login')]"));
         registerBtn.click();
         WebElement registerEmail = driver.findElement(By.id("registerEmail"));
         WebElement registerPwd = driver.findElement(By.xpath("//input[@id='registerEmail']/ancestor::div[@class='form-group']/following-sibling::div//input[@id='password']"));
         WebElement createBtn = driver.findElement(By.xpath("//button[contains(text(),'Create Account')]"));
         WebElement pageTextEle = driver.findElement(By.xpath("//div[@id='create_login']/p"));
-        WebElement ErrorEle = driver.findElement(By.xpath("//p[@class='alert alert-danger']/child::strong"));
-
 
         String expectedText = "To access your notes from anywhere and never lose them, just create an account (it will take less than a minute). If you already have an account, you can login below.";
-        String existText = "Email also exists in system. Please Login.";
         String pageText = "";
-        String errorText = "";
-        String email = "test10@me.com";
+        String currentURL = "";
+        String email = "test20@me.com";
         String password = "123456";
 
         // Redirect to Sign In page
@@ -43,20 +39,35 @@ class SignIn {
         registerEmail.sendKeys(email);
         registerPwd.sendKeys(password);
         createBtn.click();
+        currentURL = driver.getCurrentUrl();
 
-        WebElement logoutBtn = driver.findElement(By.xpath("//span[text()='Logout']"));
-        logoutBtn.click();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        if (currentURL.equals("https://anotepad.com/")) {
+            WebElement logoutBtn = driver.findElement(By.xpath("//span[text()='Logout']"));
+            logoutBtn.click();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        //Sign In exist account
-        driver.get("https://anotepad.com/create_account");
+            //Sign In new account
+            driver.get("https://anotepad.com/create_account");
+            WebElement loginEmail = driver.findElement(By.id("loginEmail"));
+            WebElement loginPwd = driver.findElement(By.xpath("//input[@id='loginEmail']/ancestor::div[@class='form-group']/following-sibling::div//input[@id='password']"));
+            WebElement loginBtn = driver.findElement(By.xpath("//button[contains(text(),'Login')]"));
+            loginEmail.sendKeys(email);
+            loginPwd.sendKeys(password);
+            loginBtn.click();
+        } else {
+            WebElement ErrorEle = driver.findElement(By.xpath("//p[@class='alert alert-danger']/child::strong"));
+            if (ErrorEle.isDisplayed()) {
 
-        WebElement loginEmail = driver.findElement(By.id("loginEmail"));
-        WebElement loginPwd = driver.findElement(By.xpath("//input[@id='loginEmail']/ancestor::div[@class='form-group']/following-sibling::div//input[@id='password']"));
-        WebElement loginBtn = driver.findElement(By.xpath("//button[contains(text(),'Login')]"));
-        loginEmail.sendKeys(email);
-        loginPwd.sendKeys(password);
-        loginBtn.click();
+                //Sign In exist account
+                driver.get("https://anotepad.com/create_account");
+                WebElement loginEmail = driver.findElement(By.id("loginEmail"));
+                WebElement loginPwd = driver.findElement(By.xpath("//input[@id='loginEmail']/ancestor::div[@class='form-group']/following-sibling::div//input[@id='password']"));
+                WebElement loginBtn = driver.findElement(By.xpath("//button[contains(text(),'Login')]"));
+                loginEmail.sendKeys(email);
+                loginPwd.sendKeys(password);
+                loginBtn.click();
+            }
+        }
 
         driver.quit();
     }
